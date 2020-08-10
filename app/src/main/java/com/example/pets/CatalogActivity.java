44614a3bfa1a1aec7e0b087804d15b350fb35b42
AlaaElden98 <34111697+AlaaElden98@users.noreply.github.com>
2020-08-10 15:20:40 +0200

    Delete All pets and modify the UI

--- app/src/main/java/com/example/pets/CatalogActivity.java
+++ app/src/main/java/com/example/pets/CatalogActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -89,8 +90,13 @@ public class CatalogActivity extends AppCompatActivity implements
         });
 
         // Kick off the loader
-        getLoaderManager().initLoader(PET_LOADER, null,  this);
+        getLoaderManager().initLoader(PET_LOADER, null, this);
     }
+
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+    }
+
     private void insertPet() {
         // Create a ContentValues object where column names are the keys,
         // and Toto's pet attributes are the values.
@@ -107,61 +113,61 @@ public class CatalogActivity extends AppCompatActivity implements
         Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
     }
 
-       @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu options from the res/menu/menu_catalog.xml file.
-            // This adds menu items to the app bar.
-            getMenuInflater().inflate(R.menu.menu_catalog, menu);
-            return true;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertPet();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllPets();
+                return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            // User clicked on a menu option in the app bar overflow menu
-            switch (item.getItemId()) {
-                // Respond to a click on the "Insert dummy data" menu option
-                case R.id.action_insert_dummy_data:
-                    insertPet();
-                    return true;
-                // Respond to a click on the "Delete all entries" menu option
-                case R.id.action_delete_all_entries:
-                    //         deleteDatabase();
-                    return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED};
 
-        @Override
-        public Loader<Cursor> onCreateLoader ( int i,  Bundle bundle) {
-            // Define a projection that specifies the columns from the table we care about.
-            String[] projection = {
-                    PetEntry._ID,
-                    PetEntry.COLUMN_PET_NAME,
-                    PetEntry.COLUMN_PET_BREED };
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                PetEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
 
-            // This loader will execute the ContentProvider's query method on a background thread
-            return new CursorLoader(this,   // Parent activity context
-                    PetEntry.CONTENT_URI,   // Provider content URI to query
-                    projection,             // Columns to include in the resulting Cursor
-                    null,                   // No selection clause
-                    null,                   // No selection arguments
-                    null);                  // Default sort order
-        }
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        // Update {@link PetCursorAdapter} with this new cursor containing updated pet data
+        mCursorAdapter.swapCursor(data);
+    }
 
-        @Override
-        public void onLoadFinished (@NonNull Loader < Cursor > loader, Cursor data){
-            // Update {@link PetCursorAdapter} with this new cursor containing updated pet data
-            mCursorAdapter.swapCursor(data);
-        }
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
 
-        @Override
-        public void onLoaderReset (@NonNull Loader < Cursor > loader) {
-            // Callback called when the data needs to be deleted
-            mCursorAdapter.swapCursor(null);
-
-        }
+    }
 
     /*private void deleteDatabase() {
 
     }*/
-    }
+}
